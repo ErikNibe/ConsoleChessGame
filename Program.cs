e37d87efdfea79a548ca6e151081bf57b296d644
Érik Eiji <eriknibe1@gmail.com>
2023-09-05 15:24:04 -0300

    feat: improving board implementation - differ white from black pieces

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using ConsoleChessGame.Entities.board;
 using ConsoleChessGame.Entities.board.Enums;
+using ConsoleChessGame.Entities.chess;
 using ConsoleChessGame.Entities.Chess;
 
 namespace ConsoleChessGame
@@ -13,8 +14,7 @@ namespace ConsoleChessGame
                 Board board = new Board(8, 8);
 
                 board.AddPiece(new Tower(board, Color.Black), new Position(0, 0));
-                board.AddPiece(new Tower(board, Color.Black), new Position(0, 9));
-                board.AddPiece(new Tower(board, Color.Black), new Position(2, 4));
+                board.AddPiece(new Tower(board, Color.White), new Position(3, 5));
 
                 Screen.PrintBoard(board);
             }
